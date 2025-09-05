4db40de8036df8842e54b969b88e8ed42e0df19a
Sunny Sharma <sunnytarunsharma67@gmail.com>
2025-09-05 15:56:24 +0530

    Add files via upload

---
+++ server/djangoapp/urls.py
@@ -0,0 +1,19 @@
+# Uncomment the imports before you add the code
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+app_name = 'djangoapp'
+urlpatterns = [
+    # # path for registration
+        path('', views.home, name='home'),
+    # path for login
+        path('about/', views.about, name='about'),
+    # path(route='login', view=views.login_user, name='login'),
+
+    # path for dealer reviews view
+
+    # path for add a review view
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
